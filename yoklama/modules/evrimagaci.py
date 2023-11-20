019daa4fe57029c80d2cff17cb6db96236d07145
Kader Miyanyedi <Kadermiyanyedi@hotmail.com>
2023-11-20 19:56:59 +0300

    feat(modules): add kızlarsoruyor, onedio and evrimagaci modules

---
+++ yoklama/modules/evrimagaci.py
@@ -0,0 +1,6 @@
+from yoklama.base import Module
+
+
+class EvrimAgaci(Module):
+    name = "evrimagaci.org"
+    url = "https://evrimagaci.org/{value}"
---
+++ yoklama/modules/onedio.py
@@ -0,0 +1,6 @@
+from yoklama.base import Module
+
+
+class Onedio(Module):
+    name = "onedio.com"
+    url = "https://onedio.com/profil/{value}"
