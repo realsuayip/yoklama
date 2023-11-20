d25e605b62ae74249b9e8a94aeb4039f8a1fff5a
Kader Miyanyedi <Kadermiyanyedi@hotmail.com>
2023-11-20 19:56:59 +0300

    feat(modules): add kızlarsoruyor, onedio and evrimagaci modules

--- yoklama/__init__.py
+++ yoklama/__init__.py
@@ -1,7 +1,10 @@
 import yoklama.modules.donanimhaber
 import yoklama.modules.eksisozluk
+import yoklama.modules.evrimagaci
 import yoklama.modules.incisozluk
 import yoklama.modules.instela
+import yoklama.modules.kizlarsoruyor
+import yoklama.modules.onedio
 import yoklama.modules.shiftdelete
 import yoklama.modules.technopat
 import yoklama.modules.teknoseyir
---
+++ yoklama/modules/kizlarsoruyor.py
@@ -0,0 +1,6 @@
+from yoklama.base import Module
+
+
+class KizlarSoruyor(Module):
+    name = "kizlarsoruyor.com"
+    url = "https://www.kizlarsoruyor.com/uye/{value}"
